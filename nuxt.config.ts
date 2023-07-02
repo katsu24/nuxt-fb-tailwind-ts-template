51accdbc675c23153eec2bcca8a973c181837a11
Katsumi Nakao <nakao.katsumi.75@gmail.com>
2023-07-02 22:17:57 +0900

    Setup Typescript

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
+  head: {
+    title: 'nuxt-fb-tailwind-ts-template',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  modules: [],
+
+  build: {},
+}
+
+export default config
